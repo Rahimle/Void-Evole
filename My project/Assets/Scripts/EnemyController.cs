1f8844484ac75c05b4c3b56498563fe3b927b56d
Amourine <bimcoi2003@gmail.com>
2025-08-25 15:52:18 +0700

    test

---
+++ My project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 2f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        // Tự hủy nếu rơi khỏi màn hình
+        if (transform.position.y < -Camera.main.orthographicSize - 1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
