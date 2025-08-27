8c60629ab87c09fcfa9cd51ca1fbed51995e2c04
Dev_Num0 <106236672+ApostleDev0@users.noreply.github.com>
2025-08-27 12:26:39 +0700

    Finish task week 1
    - Add Player, Projectile, Enemy
    - Move Projectile and Enemy to Prefabs to make prefabs
    - Delete some unused folder

---
+++ Void Evole/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // thong bao trong console khi run
+        Debug.Log("Player has spawned!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
