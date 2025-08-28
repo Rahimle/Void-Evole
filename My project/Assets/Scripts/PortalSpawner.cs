436ba70ba54e21a62f973712e18fd57514a628af
Amourine <bimcoi2003@gmail.com>
2025-08-28 21:03:05 +0700

    Portal

---
+++ My project/Assets/Scripts/PortalSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PortalSpawner : MonoBehaviour
+{
+    public GameObject portalPrefab;
+    public Transform spawnPoint;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) // Nhấn P để tạo portal
+        {
+            Instantiate(portalPrefab, spawnPoint.position, Quaternion.identity);
+        }
+    }
+}
