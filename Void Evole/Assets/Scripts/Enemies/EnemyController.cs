241f185c30f589a4073f3dc6ce971d4591898260
Dev_Num0 <106236672+ApostleDev0@users.noreply.github.com>
2025-08-29 20:16:08 +0700

    Update Enemy
    - Enemies will locate Player and move to Player

--- Void Evole/Assets/Scripts/Enemies/EnemyController.cs
+++ Void Evole/Assets/Scripts/Enemies/EnemyController.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 2f;// bien luu toc do quai
+    public float speed = 1.5f;// bien luu toc do quai
+    private Transform playerTransform; // bien luu vi tri nguoi choi
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // tim vi tri nguoi choi 
+        GameObject playerObject = GameObject.Find("Player");
+
+        // neu tim thay thi luu vi tri ]
+        if(playerObject != null)
+        {
+            playerTransform = playerObject.transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // di chuyen quai sang trai
-        // Vector2.left laf vector chi huong sang trai (-1,0)
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-        
-        if(transform.position.x < -10f)// neu ra khoi man hinh trai 
+        // kiem tra neu tim thay player
+        if (playerTransform != null)
+        {
+            // tinh toan huong di chuyen tu vi tri enemy -> player
+            // .normalized dam bao vector co lenght = 1, giup speed di chuyen on dinh
+            Vector2 direction = (playerTransform.position - transform.position).normalized;
+
+            // di chuyen enemy theo huong 
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+
+        if (transform.position.x < -10f)// neu ra khoi man hinh trai 
         {
             Destroy(gameObject);// huy 
         }
